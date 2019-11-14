197cdfa96c242a51b675b7eb484b3cdf0aafc607
Denner Marchi <denner883@hotmail.com>
2019-11-14 17:49:10 -0300

    findById implementation

--- src/model/dao/implementation/SellerDaoJDBC.java
+++ src/model/dao/implementation/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.implementation;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller obj) {
@@ -24,7 +37,38 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement statement = null;
+        ResultSet set = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE seller.Id = ?"
+            );
+            statement.setInt(1, id);
+            set = statement.executeQuery();
+            if (set.next()) {
+                Department department = new Department();
+                department.setId(set.getInt("DepartmentId"));
+                department.setName(set.getString("DepName"));
+                Seller obj = new Seller();
+                obj.setId(set.getInt("Id"));
+                obj.setName(set.getString("Name"));
+                obj.setEmail(set.getString("Email"));
+                obj.setBaseSalary(set.getDouble("BaseSalary"));
+                obj.setBirthDate(set.getDate("BirthDate"));
+                obj.setDepartment(department);
+                return obj;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(set);
+        }
     }
 
     @Override
